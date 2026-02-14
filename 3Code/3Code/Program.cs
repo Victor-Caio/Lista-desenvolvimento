8eacb2a4f0616690f4d56886db83804bb9e7cf9c
Victor-Caio <kkviruz@gmail.com>
2026-02-14 18:53:14 -0300

    Add files via upload

---
+++ 3Code/3Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maior = 0;
+
+        for (int i = 1; i <= 15; i++)
+        {
+            Console.Write($"Digite o {i}º número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+        }
+
+        Console.WriteLine($"\nO maior número digitado foi: {maior}");
+    }
+}
