7859892937dd308a802f01c663193e8b9409f90a
Victor-Caio <kkviruz@gmail.com>
2026-02-14 18:47:20 -0300

    Add files via upload

---
+++ 1Code/1Code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros pares do 211 até 250");
+            for (int i = 211; i <= 250; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
